8fbcacb12446c5b307da22208e1b2a1b9bb4006f
linyyyang <linyyyang@tencent.com>
2023-12-13 08:20:41 +0800

    chore: pnpm

--- src/router/inject.ts
+++ src/router/inject.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2023-12-09 12:02:29
- * @LastEditTime: 2023-12-13 07:51:56
+ * @LastEditTime: 2023-12-13 08:19:30
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
