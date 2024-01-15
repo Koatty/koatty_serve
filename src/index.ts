3efd4ba30d700af6b5ba319c59f460c5198fecd2
linyyyang <linyyyang@tencent.com>
2024-01-16 00:25:54 +0800

    fix: remove export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2023-12-09 12:02:29
- * @LastEditTime: 2023-12-09 15:19:53
+ * @LastEditTime: 2024-01-14 23:56:38
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
@@ -15,4 +15,3 @@ export * from "./server/terminus";
 export * from "./router/router";
 export * from "./router/mapping";
 export * from "./router/params";
-export * from "./router/payload";
