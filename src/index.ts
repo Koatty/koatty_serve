73c5476b0635ff825b90c7b00a1f013ade467fc2
linyyyang <linyyyang@tencent.com>
2024-01-16 01:04:17 +0800

    refactor: router 分离

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Usage: 
  * @Author: richen
  * @Date: 2023-12-09 12:02:29
- * @LastEditTime: 2024-01-14 23:56:38
+ * @LastEditTime: 2024-01-16 00:52:21
  * @License: BSD (3-Clause)
  * @Copyright (c): <richenlin(at)gmail.com>
  */
@@ -11,7 +11,3 @@
 // export
 export * from "./server/serve";
 export * from "./server/terminus";
-// export
-export * from "./router/router";
-export * from "./router/mapping";
-export * from "./router/params";
