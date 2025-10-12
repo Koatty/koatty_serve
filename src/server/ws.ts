aa1c1065383c8589ba56d511c1eba517268c50e0
richenlin <richenlin@gmail.com>
2025-10-12 13:45:09 +0800

    feat: add Koatty-style server startup logs with protocol and URL info for all server types

--- src/server/ws.ts
+++ src/server/ws.ts
@@ -466,6 +466,13 @@ export class WsServer extends BaseServer<WebSocketServerOptions> {
     this.ensureUpgradeHandlersAreBound();
 
     this.httpServer.listen(this.options.port, this.options.hostname, () => {
+      const protocolUpper = this.options.protocol.toUpperCase();
+      const urlProtocol = this.options.protocol.toLowerCase();
+      const serverUrl = `${urlProtocol}://${this.options.hostname || '127.0.0.1'}:${this.options.port}/`;
+      
+      // 输出 Koatty 格式的启动日志
+      this.logger.info(`Server: ${protocolUpper} running at ${serverUrl}`, { traceId });
+      
       this.logger.info('Server started', { traceId }, {
         address: `${this.options.hostname}:${this.options.port}`,
         hostname: this.options.hostname,
